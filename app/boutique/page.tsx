8b493092bc4450ec4f4af8ab04fe0281ea88eb18
ekilalumande7-crypto <ekilalumande7@gmail.com>
2026-03-29 19:21:45 +0200

    Updated package.json

--- app/boutique/page.tsx
+++ app/boutique/page.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-export const dynamic = "force-dynamic";
-
 import { useState } from "react";
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
